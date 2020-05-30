8036466ccec83bfb5d0351a455e127587ce36c5a
Freedy <21914103@zju.edu.cn>
2020-05-30 14:47:15 +0800

    Vue初体验

---
+++ src/Demo.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    {{n}}
+    <button @click="add" class="red">+1</button>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      n: 0
+    };
+  },
+  methods: {
+    add() {
+      this.n += 1;
+    }
+  }
+};
+</script>
+
+
+<style scoped>
+.red {
+  color: red;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,15 @@
+// import Vue from 'vue'
+// import App from './App.vue'
+
+// Vue.config.productionTip = false
+import Demo from './Demo.vue'
+
+
+new Vue({
+  el: '#app',
+  render(h) {
+    return h(Demo)
+  }
+})
+
+console.log(window.Vue)
